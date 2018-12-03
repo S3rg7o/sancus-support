2b4bfb2f5e3324bad5a246ea493dff70ac2dc99f
S3rg7o <42998452+S3rg7o@users.noreply.github.com>
2018-12-03 12:07:50 +0100

    Update sm_io.c

--- src/sm-io/sm_io.c
+++ src/sm-io/sm_io.c
@@ -68,9 +68,8 @@ __attribute__((optimize("-O3")))
  * a stack frame: https://sourceforge.net/p/mspgcc/support-requests/27/ */
 void violation_isr(void)
 {
-    asm(   "mov &__unprotected_sp, r1                  \n\t"   
-           /*"call #stop_violation                       \n\t"*/
-           //"reti"
-        
+    asm(   puts("\t--> SM VIOLATION DETECTED; exiting...\n");
+          //"mov &__unprotected_sp, r1                  \n\t"   
+           /*"call #stop_violation                       \n\t"*/       
        );
 }
